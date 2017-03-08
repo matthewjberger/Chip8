f564fe1fa51d6c582e5b5b6742934998795a8a87
Travis CI User <travis@example.org>
2017-03-08 19:54:25 +0000

    Documentation for matthewjberger/Chip8@a60d788

---
+++ backend/instructions/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Instructions","This contains all of the instructions in the Chip-8 instruction set."]],"struct":[["Address","A 16-bit address `NNN`"],["Byte","8-bit information in an opcode. `NN`"],["Nibble","4-bit information in an opcode. `N`"]],"type":[["Vx","Represents an arbitrary 8-bit register."],["Vy","Represents an arbitrary 8-bit register."]]});
